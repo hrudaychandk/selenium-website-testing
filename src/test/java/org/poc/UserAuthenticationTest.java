f9b13344153961273c84eca65866841ae1f75345
Hruday <hkatakam@virtusa.com>
2019-12-19 04:33:32 -0600

    changed chromedriver location

--- src/test/java/org/poc/UserAuthenticationTest.java
+++ src/test/java/org/poc/UserAuthenticationTest.java
@@ -1,6 +1,5 @@
 package org.poc;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -22,15 +21,16 @@ public class UserAuthenticationTest {
         ChromeOptions chromeOptions = new ChromeOptions();
         WebDriver driver = new ChromeDriver(chromeOptions);
         // Opens a website hosted on below machine
-        driver.get("https://localhost:3001");
+        //driver.get("https://localhost:3001");
+        driver.get("https://www.google.com");
         // Setting an implicit time out for loading of different elements on the website
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         // Find the username element and pass the username
-        driver.findElement(By.id("email")).sendKeys("");
+        /*driver.findElement(By.id("email")).sendKeys("");
         // Find the password element and pass the password
         driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("");
         // Find the submit button and click to authenticate the user
-        driver.findElement(By.id("u_0_2")).click();
+        driver.findElement(By.id("u_0_2")).click();*/
         // TODO: Assert
 
     }
