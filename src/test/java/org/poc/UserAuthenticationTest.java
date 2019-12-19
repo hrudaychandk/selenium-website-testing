2c86072bd7955b60e11aebba6db81b0703223e78
Hruday <hkatakam@virtusa.com>
2019-12-19 05:20:04 -0600

    Added automated test for website

--- src/test/java/org/poc/UserAuthenticationTest.java
+++ src/test/java/org/poc/UserAuthenticationTest.java
@@ -1,5 +1,6 @@
 package org.poc;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -23,18 +24,19 @@ public class UserAuthenticationTest {
         chromeOptions.addArguments("--no-sandbox");
         WebDriver driver = new ChromeDriver(chromeOptions);
         // Opens a website hosted on below machine
-        //driver.get("https://localhost:3001");
-        driver.get("https://www.google.com");
+        driver.get("http://localhost:3001");
+        //driver.get("https://www.google.com");
         // Setting an implicit time out for loading of different elements on the website
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        System.out.println(driver.getTitle());
         // Find the username element and pass the username
-        /*driver.findElement(By.id("email")).sendKeys("");
+        driver.findElement(By.name("login")).sendKeys("devops");
         // Find the password element and pass the password
-        driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("");
+        driver.findElement(By.name("password")).sendKeys("test");
         // Find the submit button and click to authenticate the user
-        driver.findElement(By.id("u_0_2")).click();*/
-        // TODO: Assert
-
+        driver.findElement(By.name("click")).click();
+        //Quit the driver
+        driver.quit();
     }
 
     @AfterTest
